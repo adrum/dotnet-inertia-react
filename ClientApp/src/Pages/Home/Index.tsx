61b36a277b271e91977579b7b8c0e88dece6f4d8
Austin Drummond <adrum0x7@gmail.com>
2023-04-16 15:42:28 -0400

    added some lightweight documentation

--- ClientApp/src/Pages/Home/Index.tsx
+++ ClientApp/src/Pages/Home/Index.tsx
@@ -55,10 +55,9 @@ export default function Index() {
         </li>
         <li>
           <strong>Development server integration</strong>. In development mode,
-          the development server from <code>create-react-app</code> runs in the
-          background automatically, so your client-side resources are
-          dynamically built on demand and the page refreshes when you modify any
-          file.
+          the development server from <code>Vite</code> runs in the background
+          automatically, so your client-side resources are dynamically built on
+          demand and the page refreshes when you modify any file.
         </li>
         <li>
           <strong>Efficient production builds</strong>. In production mode,
@@ -69,9 +68,9 @@ export default function Index() {
       </ul>
       <p>
         The <code>ClientApp</code> subdirectory is a standard React application
-        based on the <code>create-react-app</code> template. If you open a
-        command prompt in that directory, you can run <code>npm</code> commands
-        such as <code>npm test</code> or <code>npm install</code>.
+        based on the <code>Vite+Inertia+Typescript</code> template. If you open
+        a command prompt in that directory, you can run <code>npm</code>{" "}
+        commands such as <code>npm test</code> or <code>npm install</code>.
       </p>
     </div>
   );
---
+++ README.md
@@ -0,0 +1,32 @@
+# .NET Core + Inertia + React Boilerplate
+
+This is an example project for utilizing [InertiaCore](https://github.com/kapi2289/InertiaCore) with [React](https://react.dev/) and [Vite](https://vitejs.dev/).
+
+## Getting Started
+
+```bash
+# Clone the repo
+git clone {this repo url}
+
+cd dotnet-inertia-react
+
+dotnet restore
+
+dotnet build
+
+dotnet publish
+```
+
+## Development
+
+```bash
+cd ClientApp
+npm run dev
+
+# in another terminal
+dotnet watch run
+```
+
+## VSCode
+
+Helpful vscode tasks also exits. The "Watch All" task automates the steps in the Development section above.
